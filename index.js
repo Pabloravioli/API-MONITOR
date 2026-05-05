7a913a0cc5f89ce26ea5b159b7beb2f03514ae1b
unknown <pabloespinosa888@gmail.com>
2026-05-04 23:09:44 -0300

    Se modificaron los archivos json

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get("/hola",(req, res)=> {
     res.send("Hola desde express!");
 })
 
+app.use(express.static("urls"));
+
+app.get("/api/data", (req, res) => {
+
+    res.json({ message: "https://www.google.com.ar" })
+});
+
+let message = [];
+
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
