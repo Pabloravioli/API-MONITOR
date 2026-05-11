b599693bcaae84c256abdd2e7be71cc88e304396
unknown <pabloespinosa888@gmail.com>
2026-05-11 01:20:04 -0300

    Agrego la verificacion de URL

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(express.json());
 
+
+
 let messages = [];
 
 app.get("/api/message", (req, res) => {
@@ -17,6 +19,26 @@ app.post("/api/message", (req, res) => {
     console.log(messages)
     res.status(201).json({ success: true, message: "Recibido" });
 });
+
+app.get("/check", async (req, res) => {
+
+    const url = req.query.url;
+
+    if(!url) {
+        return res.status(400).json({ error: "URL is required" });
+    }
+
+    try {
+        const response = await fetch(url, {method: "HEAD"});
+        const status = response.status;
+        res.json({ url, status });
+    } catch (error) {
+        res.status(500).json({url, error: "Failed to check URL" });
+    }
+});
+
+
+
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
